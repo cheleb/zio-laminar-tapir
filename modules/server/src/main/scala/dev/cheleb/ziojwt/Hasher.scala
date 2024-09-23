ff49b371f51473185fad74be1f25c41f9db1c061
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-09-23 21:17:07 +0200

    Refactor documentation and add utility object for password hashing

--- modules/server/src/main/scala/dev/cheleb/ziojwt/Hasher.scala
+++ modules/server/src/main/scala/dev/cheleb/ziojwt/Hasher.scala
@@ -4,6 +4,9 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 import java.security.SecureRandom
 
+/** Hasher is a utility object to hash and validate passwords.
+  *   - It uses PBKDF2WithHmacSHA512 as the hashing algorithm.
+  */
 object Hasher {
 
   private val PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA512"
@@ -11,17 +14,22 @@ object Hasher {
   private val SALT_BYTE_SIZE = 24
   private val HASH_BYTE_SIZE = 24
 
-  val skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM)
+  /** The SecretKeyFactory is a factory for secret keys.
+    */
+  private val skf: SecretKeyFactory =
+    SecretKeyFactory.getInstance(PBKDF2_ALGORITHM)
 
+  /** Hashes a password with a random salt.
+    */
   private def pbkdf2(
       password: Array[Char],
       salt: Array[Byte],
       iterations: Int,
       nBytes: Int
-  ): Array[Byte] =
-
+  ): Array[Byte] = {
     val keySpec = PBEKeySpec(password, salt, iterations, nBytes * 8)
     skf.generateSecret(keySpec).getEncoded()
+  }
 
   private def toHex(array: Array[Byte]): String =
     array.map("%02X".format(_)).mkString
@@ -35,14 +43,26 @@ object Hasher {
       range.foldLeft(a1.length ^ a2.length)((acc, i) => acc | (a1(i) ^ a2(i)))
     diff == 0
 
-  def generatedHash(password: String): String =
+  /** Generates a hash from a password.
+    *
+    * @param password
+    * @return
+    */
+  def generatedHash(password: String): String = {
     val rng: SecureRandom = new SecureRandom()
     val salt: Array[Byte] = Array.ofDim[Byte](SALT_BYTE_SIZE)
     rng.nextBytes(salt) // fill the salt with SALT_BYTE_SIZE random bytes
     val hashBytes =
       pbkdf2(password.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE)
     s"$PBKDF2_ITERATIONS:${toHex(salt)}:${toHex(hashBytes)}"
+  }
 
+  /** Validates a password against a hash.
+    *
+    * @param string
+    * @param hash
+    * @return
+    */
   def validateHash(string: String, hash: String): Boolean =
     val hashSegments = hash.split(":")
     val iterations = hashSegments(0).toInt
--- modules/shared/src/main/scala/dev/cheleb/ziojwt/WithToken.scala
+++ modules/shared/src/main/scala/dev/cheleb/ziojwt/WithToken.scala
@@ -2,7 +2,15 @@ package dev.cheleb.ziojwt
 
 import sttp.model.Uri
 
+/** A trait to represent a token with an expiration date.
+  */
 trait WithToken {
-  def token: String
+
+  /** The token itself.
+    */
+  val token: String
+
+  /** The expiration date of the token.
+    */
   val expiration: Long
 }
