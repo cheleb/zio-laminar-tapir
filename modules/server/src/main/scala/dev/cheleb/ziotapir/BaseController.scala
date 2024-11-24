be1ab2e0bb5d3558ae2d4aab1687a15ba450b0e8
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-11-24 23:04:29 +0100

    Streaming responses support

--- modules/server/src/main/scala/dev/cheleb/ziotapir/BaseController.scala
+++ modules/server/src/main/scala/dev/cheleb/ziotapir/BaseController.scala
@@ -1,10 +1,16 @@
 package dev.cheleb.ziotapir
 
+import sttp.tapir.ztapir.*
 import sttp.tapir.server.ServerEndpoint
+import sttp.capabilities.zio.ZioStreams
+
 import zio.Task
 
 trait BaseController {
 
-  val routes: List[ServerEndpoint[Any, Task]]
+  val routes: (
+      List[ServerEndpoint[Any, Task]],
+      List[ZServerEndpoint[Any, ZioStreams]]
+  )
 
 }
