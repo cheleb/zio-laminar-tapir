639225f088f0f69cb83f67b793099ddffabb6ae5
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-12-12 22:28:57 +0100

    Simplify (#43)
    * feat(routes): add Routes trait and update BaseController with routes and streamRoutes
    
    * feat(routes): enhance Routes trait with gatherRoutes method and rename securedServerLogic to zServerAuthenticatedLogic

---
+++ modules/server/src/main/scala/dev/cheleb/ziotapir/Routes.scala
@@ -0,0 +1,35 @@
+package dev.cheleb.ziotapir
+
+import zio.*
+import sttp.tapir.server.ServerEndpoint
+
+/** A trait that provides a method to gather all the routes from a controllers.
+  *
+  * The method `gatherRoutes` takes a function that selects the routes from a
+  * controller, the type parameter `C` is the type of the context that the
+  * routes require (Streams, WebSockets, etc.).
+  *
+  * @tparam C
+  *   The type of the context that the routes require.
+  */
+trait Routes {
+
+  /** Gathers all the routes from a list of controllers.
+    *
+    * @param select
+    *   A function that selects the routes from a controller.
+    * @param controllers
+    *   A list of controllers.
+    * @tparam C
+    *   The type of the context that the routes require.
+    * @return
+    *   A list of server endpoints.
+    */
+  protected def gatherRoutes[C](
+      select: BaseController => List[ServerEndpoint[C, Task]]
+  )(
+      controllers: List[BaseController]
+  ): List[ServerEndpoint[C, Task]] =
+    controllers flatMap select
+
+}
