9c00be4ed206e5278a921020b923ad2b773d60f8
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-01-01 18:17:47 +0100

    chore(deps): bump tapir to v1.13.4, zio to v2.1.24, and zioPrelude to v1.0.0-RC44; update Scala CLI shebang (#141)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,12 +7,12 @@ object Dependencies {
     val laminar = "17.2.1"
     val logbackClassic = "1.5.23"
     val webawesome = "3.0.0"
-    val tapir = "1.13.3"
+    val tapir = "1.13.4"
     val sttp = "4.0.13"
     val sttpModelCore = "1.7.17"
-    val zio = "2.1.23"
+    val zio = "2.1.24"
     val zioJson = "0.7.45"
-    val zioPrelude = "1.0.0-RC42"
+    val zioPrelude = "1.0.0-RC44"
   }
 
   val coreDependencies =
