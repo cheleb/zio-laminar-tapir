b02b27e4a75af9ccda95928b8c40d2d1d76a491b
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-01-25 20:34:40 +0100

    Refactor websocket connection logic into separate functions for improved readability

--- examples/client/src/main/scala/demo/websocket.scala
+++ examples/client/src/main/scala/demo/websocket.scala
@@ -14,66 +14,24 @@ import zio.stream.ZStream
 import sttp.ws.WebSocketFrame
 import sttp.ws.WebSocketFrame.Text
 
-val echoWebsocket: Var[Uri] = Var(
+private val echoWebsocket: Var[Uri] = Var(
   Uri.unsafeParse("https://echo.websocket.org")
 )
 
 def websocket =
+  // Hub to send messages to the WebSocket
+  // Initially None, meaning not connected
+  // When connected, it will hold a Hub[WebSocketFrame]
   val hubVar: Var[Option[Hub[WebSocketFrame]]] = Var(None)
 
   div(
     cls := "spaced",
     children <-- hubVar.signal.map:
       case None =>
-        List(
-          chooseServer(),
-          ZButton("Connect", "plug", echoWebsocket.now()):
-            for {
-              _ <- result.zEmit("Connecting to WebSocket...")
-
-              ws <- WebsocketEndpoint.echo(())
-
-              hub <- Hub.unbounded[WebSocketFrame]
-
-              _ = hubVar.set(Some(hub))
-
-              _ <- ws(
-                ZStream
-                  .fromHubWithShutdown(hub)
-                  .tap(msg => result.zEmit(s"Sending: $msg"))
-              )
-                .runForeach {
-                  case Text(payload = payload) =>
-                    result.zEmit(s"Received: $payload")
-                  case _ => ZIO.unit
-                }
-
-              _ <- result.zEmit("WebSocket closed.")
-
-            } yield ()
-        )
+        openConnectionUI(hubVar)
 
       case Some(hub) =>
-        val message = Var("")
-
-        List(
-          span(
-            cls := "flex",
-            Input(
-              _.value <-- message.signal,
-              _.placeholder := "Type a message to send",
-              _.onInput.mapToValue --> message
-            )(
-            ),
-            ZButton("Send", "envelope"):
-              hub.sendText(message.now())
-          ),
-          ZButton("Close socket", "close"):
-            for
-              _ = hubVar.set(None)
-              _ <- hub.closeGracefully
-            yield ()
-        )
+        connnectedUI(hubVar, hub)
   ).withSnippet:
     """|for
        |  // ws is a function OutputStream => InputStream 
@@ -101,7 +59,7 @@ def websocket =
        |yield ()
   """
 
-def chooseServer(): Dropdown.Element =
+private def chooseServer(): Dropdown.Element =
   Dropdown(
     _.onSelect
       .map(
@@ -130,4 +88,58 @@ def chooseServer(): Dropdown.Element =
     DropdownItem(_.value := "ws://localhost:8080")(
       "ws://localhost:8080"
     )
+  )
+
+private def openConnectionUI(hubVar: Var[Option[Hub[WebSocketFrame]]]) =
+  List(
+    chooseServer(),
+    ZButton("Connect", "plug", echoWebsocket.now()):
+      for {
+        _ <- result.zEmit("Connecting to WebSocket...")
+
+        ws <- WebsocketEndpoint.echo(())
+
+        hub <- Hub.unbounded[WebSocketFrame]
+
+        _ = hubVar.set(Some(hub))
+
+        _ <- ws(
+          ZStream
+            .fromHubWithShutdown(hub)
+            .tap(msg => result.zEmit(s"Sending: $msg"))
+        )
+          .runForeach {
+            case Text(payload = payload) =>
+              result.zEmit(s"Received: $payload")
+            case _ => ZIO.unit
+          }
+
+        _ <- result.zEmit("WebSocket closed.")
+
+      } yield ()
+  )
+
+private def connnectedUI(
+    hubVar: Var[Option[Hub[WebSocketFrame]]],
+    hub: Hub[WebSocketFrame]
+) =
+  val message = Var("")
+
+  List(
+    span(
+      cls := "flex",
+      Input(
+        _.value <-- message.signal,
+        _.placeholder := "Type a message to send",
+        _.onInput.mapToValue --> message
+      )(
+      ),
+      ZButton("Send", "envelope"):
+        hub.sendText(message.now())
+    ),
+    ZButton("Close socket", "close"):
+      for
+        _ = hubVar.set(None)
+        _ <- hub.closeGracefully
+      yield ()
   )
