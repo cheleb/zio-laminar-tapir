637f6ca0535045a5c54dea5b680d17945381a4c3
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-02-08 19:39:27 +0100

    feat: ZStream to JSON lines conversion (#170)

--- modules/server/src/main/scala/dev/cheleb/ziotapir/package.scala
+++ modules/server/src/main/scala/dev/cheleb/ziotapir/package.scala
@@ -4,19 +4,31 @@ import zio.*
 import zio.json.*
 import zio.stream.*
 
-/**
-  * This package contains utilities for working with Tapir and ZIO, such as converting ZStreams to JSON lines streams.
+/** This package contains utilities for working with Tapir and ZIO, such as
+  * converting ZStreams to JSON lines streams.
   */
 
-/**
-  * Extension methods for working with ZStreams in the context of Tapir and ZIO.
-  */  
+/** Extension methods for working with ZStreams in the context of Tapir and ZIO.
+  */
+extension [R, A: JsonEncoder](stream: ZStream[R, Throwable, A])
+
+  /** Converts a ZStream of JSON-encodable values to a ZStream of bytes
+    * representing JSON lines.
+    *
+    * @return
+    *   A ZStream of bytes representing JSON lines.
+    */
+  def toJsonLinesStream: ZStream[R, Throwable, Byte] =
+    stream >>> JsonEncoder[A].encodeJsonLinesPipeline >>> ZPipeline
+      .map[Char, Byte](_.toByte)
+
 extension [R, A: JsonEncoder](z: RIO[R, ZStream[R, Throwable, A]])
 
-  /**
-    * Converts a ZStream of JSON-encodable values to a ZStream of bytes representing JSON lines.
+  /** Converts a ZStream of JSON-encodable values to a ZStream of bytes
+    * representing JSON lines.
     *
-    * @return A ZStream of bytes representing JSON lines.
+    * @return
+    *   A ZStream of bytes representing JSON lines.
     */
   def toJsonLinesStream: RIO[R, ZStream[R, Throwable, Byte]] =
     z.map(stream =>
