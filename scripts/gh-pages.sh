46d4940d3ee7b5fb63ec43bd98cf69879a4c43e2
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-04-19 22:01:45 +0200

    update website (#202)
    * fix: update build commands in gh-pages script to use bun instead of npm
    
    * fix: remove unnecessary blank line in index.md

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,7 +1,7 @@
 export VERSION=`git describe --tags --abbrev=0 | sed "s/v//"`
 echo "Documentation version: $VERSION"
 pushd examples/client
-npm ci
-npm run build
+bun ci
+bun run build
 popd
 sbt website
